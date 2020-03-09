de23755ad8da619c322f83694e976786fae5ef93
Zach Mommaerts <zmommaerts@gmail.com>
2020-03-09 10:59:26 -0500

    added a donor class to access the api

---
+++ StMarys Donor/DonorAPIClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace StMarys_Donor
+{
+    public class DonorAPIClient
+    {
+        public DonorAPIClient(HttpClient client)
+        {
+            Client = client;
+        }
+
+        public HttpClient Client { get; set; }
+    }
+}
