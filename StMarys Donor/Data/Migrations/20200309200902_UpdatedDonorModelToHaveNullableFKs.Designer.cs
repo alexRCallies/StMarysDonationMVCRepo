1cbe664051f0ffdf3685f0928401d8296c8b3311
Andrew Baird <baird.andrew4@gmail.com>
2020-03-09 15:16:21 -0500

    Removed Health Information Controller and Views, Made FKs of Address Id and HealthInformation nullable and remigrated, Got the Create Method in the DonorsController to post a new donor to the Web API

--- StMarys Donor/Data/Migrations/20200309200902_UpdatedDonorModelToHaveNullableFKs.Designer.cs
+++ StMarys Donor/Data/Migrations/20200309200902_UpdatedDonorModelToHaveNullableFKs.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using St.Marys_Donor.Data;
 
 namespace St.Marys_Donor.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200309200902_UpdatedDonorModelToHaveNullableFKs")]
+    partial class UpdatedDonorModelToHaveNullableFKs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
