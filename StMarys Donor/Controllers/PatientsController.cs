bc399054e73424ac920f67cc5e83d5d79eab568b
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-03-09 15:34:27 -0500

    profile pic shows up in details page

--- StMarys Donor/Controllers/PatientsController.cs
+++ StMarys Donor/Controllers/PatientsController.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using St.Marys_Donor.Data;
 using St.Marys_Donor.Models;
-using St.Marys_Donor.Models;
 using St.Marys_Donor.ViewModels;
 
 namespace St.Marys_Donor.Controllers
