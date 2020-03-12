dba5a6c0184dce375d42676f2399691025518596
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-03-12 15:53:19 -0500

    Verification Working

---
+++ StMarys Donor/ViewModels/HospitalVerification.cs
@@ -0,0 +1,13 @@
+﻿using St.Marys_Donor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StMarys_Donor.ViewModels
+{
+    public class HospitalVerification
+    {
+        public List<Patient> Patients { get; set; }
+    }
+}
